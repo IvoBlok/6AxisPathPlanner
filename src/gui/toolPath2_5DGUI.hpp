26ed09155f09488e7cdd85051c9a82e6dbb99a41
Ivo Blok <ivoblokdoorn@gmail.com>
2025-07-28 13:47:51 +0200

    removed unnecessary comments from toolpath GUI

--- src/gui/toolPath2_5DGUI.hpp
+++ src/gui/toolPath2_5DGUI.hpp
@@ -23,18 +23,13 @@ public:
         if (isOpen) {
             auto& objects = renderer.loadedObjects;
 
-            // Convert list to display vectors
             std::vector<std::string> objectNames;
             for (auto& obj : objects)
                 objectNames.push_back(obj->name);
             
-            // First dropdown: Start Plane selection
             handleDropdown("facePassPlaneSelectGroup", "Select Plane:", objectNames, selectedIndex1);
-            
-            // Second dropdown: Stock selection
             handleDropdown("facePassStockSelectGroup", "Select Stock:", objectNames, selectedIndex2);
             
-            // Aditional toolpath info
             ImGui::InputDouble("Traverse height [mm]##facePass", &info.safeTraverseHeight);
             ImGui::InputDouble("Stepover [mm]##facePass", &info.stepOver);
 
@@ -89,21 +84,14 @@ public:
         if (isOpen) {
             auto& objects = renderer.loadedObjects;
 
-            // Convert list to display vectors
             std::vector<std::string> objectNames;
             for (auto& obj : objects)
                 objectNames.push_back(obj->name);
 
-            // First dropdown: Start Plane selection
             handleDropdown("surfacePassStartPlaneSelectGroup", "Select Start Plane:", objectNames, selectedIndex1);
-
-            // Second dropdown: End Plane selection
             handleDropdown("surfacePassEndPlaneSelectGroup", "Select End Plane:", objectNames, selectedIndex2);
-
-            // Third dropdown: Shape selection
             handleDropdown("surfacePassShapeSelectGroup", "Select Shape:", objectNames, selectedIndex3);
 
-            // additional toolpath settings
             ImGui::InputDouble("Traverse height [mm]##surfacePass", &info.safeTraverseHeight);
             ImGui::InputDouble("Tool radius [mm]##surfacePass", &info.toolRadius);
             ImGui::InputDouble("Depth of Cut [mm]##surfacePass", &info.depthOfCut);
@@ -164,30 +152,21 @@ public:
         if (isOpen) {
             auto& objects = renderer.loadedObjects;
 
-            // Convert list to display vectors
             std::vector<std::string> objectNames;
             for (auto& obj : objects)
                 objectNames.push_back(obj->name);
 
-            // First dropdown: Start Plane selection
             handleDropdown("clearingPassStartPlaneSelectGroup", "Select Start Plane:", objectNames, selectedIndex1);
-
-            // Second dropdown: End Plane selection
             handleDropdown("clearingPassEndPlaneSelectGroup", "Select End Plane:", objectNames, selectedIndex2);
-
-            // Third dropdown: Shape selection
             handleDropdown("clearingPassShapeSelectGroup", "Select Shape:", objectNames, selectedIndex3);
-
-            // Fourth dropdown: Stock selection
             handleDropdown("clearingPassStockSelectGroup", "Select Stock:", objectNames, selectedIndex4);
 
-            // additional toolpath settings
             ImGui::InputDouble("Traverse height [mm]##clearingPass", &info.safeTraverseHeight);
             ImGui::InputDouble("Tool radius [mm]##clearingPass", &info.toolRadius);
             ImGui::InputDouble("Depth of Cut [mm]##clearingPass", &info.depthOfCut);
             ImGui::InputDouble("Stepover [mm]##clearingPass", &info.stepOver);
 
-            if (ImGui::Button("Generate!##clearingPass")) {
+            if (ImGui::Button("Generate##clearingPass")) {
                 if(selectedIndex1 != -1 && selectedIndex2 != -1 && selectedIndex3 != -1 && selectedIndex4 != -1) {
                     auto startPlaneObj = objects.begin();
                     std::advance(startPlaneObj, selectedIndex1);

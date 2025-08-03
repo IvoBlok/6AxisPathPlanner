1c13051184b53115ee63b68e8ca1da41bc31ff54
Ivo Blok <ivoblokdoorn@gmail.com>
2025-08-03 17:54:45 +0200

    fixed incorrect transparency for single objects

--- src/renderer/core/RenderEngine.cpp
+++ src/renderer/core/RenderEngine.cpp
@@ -545,7 +545,7 @@ void RenderEngine::VulkanInternals::createTriangleBasedPipeline() {
 
     rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
     rasterizer.lineWidth = 1.0f;
-    rasterizer.cullMode = VK_CULL_MODE_NONE; // required so that planes render from both sides
+    rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
     rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
 
     rasterizer.depthBiasEnable = VK_FALSE;
@@ -567,7 +567,7 @@ void RenderEngine::VulkanInternals::createTriangleBasedPipeline() {
     depthStencil.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
     depthStencil.depthTestEnable = VK_TRUE;
     depthStencil.depthWriteEnable = VK_TRUE;
-    depthStencil.depthCompareOp = VK_COMPARE_OP_LESS;
+    depthStencil.depthCompareOp = VK_COMPARE_OP_LESS_OR_EQUAL;
     depthStencil.depthBoundsTestEnable = VK_FALSE;
     depthStencil.stencilTestEnable = VK_FALSE;
 
@@ -1011,7 +1011,6 @@ void RenderEngine::VulkanInternals::recordCommandBuffer(std::list<std::shared_pt
     renderPassInfo.framebuffer = swapChainFramebuffers[imageIndex];
     renderPassInfo.renderArea.offset = { 0, 0 };
     renderPassInfo.renderArea.extent = swapChainExtent;
-    VkClearValue clearColor = { {{0.0f, 0.0f, 0.0f, 1.0f}} };
     renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
     renderPassInfo.pClearValues = clearValues.data();
 
